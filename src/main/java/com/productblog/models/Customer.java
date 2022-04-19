e23eb5f376fffb4ca66e5662faaa7147b100e4be
atumar <almustapha.tukur@decagon.dev>
2022-04-19 16:42:54 +0100

    customer model and dto created

--- src/main/java/com/productblog/models/Customer.java
+++ src/main/java/com/productblog/models/Customer.java
@@ -1,4 +1,37 @@
 package com.productblog.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class Customer {
+    @Id
+    @SequenceGenerator(
+            name ="customer_sequence",
+            sequenceName = "customer_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_sequence"
+    )
+    private Long id;
+    @NotNull(message = "first name is required")
+    private String firstName;
+    @NotNull(message = "last name is required")
+    private String lastName;
+    @Email
+    @Column(name = "email_address",nullable = false)
+    private String email;
+    private String password;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
 }
