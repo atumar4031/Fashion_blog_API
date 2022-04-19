8c16ae7c0488cd3afd349d52b3ea5afc89946110
atumar <almustapha.tukur@decagon.dev>
2022-04-19 16:04:30 +0100

    Product blog tracker

---
+++ src/test/java/com/productblog/ProductBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductBlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
